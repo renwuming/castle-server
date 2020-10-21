cc11afea84331052b9b70e680489563f9f9b08f6
任天赋 <rentianfu@xiaomi.com>
2020-10-21 23:07:27 +0800

    fix: 🐛 回合时间修改为30s

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -16,9 +16,9 @@ export default (appInfo: EggAppInfo) => {
   config.autoAuth = process.env.AUTO_AUTH;
 
   // 每回合限时
-  config.ROUND_TIME_LIMIT = 20 * 1000;
+  config.ROUND_TIME_LIMIT = 30 * 1000;
   // 玩家离线限制
-  config.OFFLINE_TIME_LIMIT = 5 * 1000;
+  config.OFFLINE_TIME_LIMIT = 2 * 1000;
   // 玩家进入托管的离线时间限制
   config.AUTOMATIC_TIME_LIMIT = 2 * 60 * 1000;
 
