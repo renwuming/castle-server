5c41088b6e52221ce9f9652e8d246226d94a04c2
任天赋 <rentianfu@xiaomi.com>
2020-10-31 18:38:26 +0800

    feat: 🎸 更新card图片资源

--- src/lib/gameHelper.ts
+++ src/lib/gameHelper.ts
@@ -5,32 +5,32 @@ type PropConfig = {
 
 export const maidservantRole: Prop = {
   name: "女佣",
-  imgUrl: "https://cdn.renwuming.cn/static/escape/r-4.jpg",
-  imgHdUrl: "https://cdn.renwuming.cn/static/escape/r-4-hd.jpg",
+  imgUrl: "https://cdn.renwuming.cn/static/escape/cards/r-4.png",
+  imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/r-4-hd.jpg",
   key: "r-4",
   status: -1,
 };
 
 export const knightRole: Prop = {
   name: "叛军骑士",
-  imgUrl: "https://cdn.renwuming.cn/static/escape/r-1.jpg",
-  imgHdUrl: "https://cdn.renwuming.cn/static/escape/r-1-hd.jpg",
+  imgUrl: "https://cdn.renwuming.cn/static/escape/cards/r-1.png",
+  imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/r-1-hd.jpg",
   key: "r-1",
   status: -1,
 };
 
 export const magicDisarm: Prop = {
   name: "缴械",
-  imgUrl: "https://cdn.renwuming.cn/static/escape/m-1.jpg",
-  imgHdUrl: "https://cdn.renwuming.cn/static/escape/m-1-hd.jpg",
+  imgUrl: "https://cdn.renwuming.cn/static/escape/cards/m-1.png",
+  imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/m-1-hd.jpg",
   key: "m-1",
   status: -1,
 };
 
 export const magicFreeze: Prop = {
   name: "冰弹",
-  imgUrl: "https://cdn.renwuming.cn/static/escape/m-2.jpg",
-  imgHdUrl: "https://cdn.renwuming.cn/static/escape/m-2-hd.jpg",
+  imgUrl: "https://cdn.renwuming.cn/static/escape/cards/m-2.png",
+  imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/m-2-hd.jpg",
   key: "m-2",
   status: -1,
 };
@@ -40,8 +40,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "流星锤",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/e-1.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/e-1-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/e-1.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/e-1-hd.jpg",
       key: "e-1",
       status: -1,
     },
@@ -50,8 +50,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "刃盾",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/e-2.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/e-2-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/e-2.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/e-2-hd.jpg",
       key: "e-2",
       status: -1,
     },
@@ -60,8 +60,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "安卡符",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/e-3.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/e-3-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/e-3.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/e-3-hd.jpg",
       key: "e-3",
       status: -1,
     },
@@ -70,8 +70,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "小马哥",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/e-4.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/e-4-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/e-4.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/e-4-hd.jpg",
       key: "e-4",
       status: -1,
     },
@@ -90,8 +90,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向东3格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-1.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-1-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-1.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-1-hd.jpg",
       key: "d-1",
       status: 3,
     },
@@ -100,8 +100,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向东2格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-1.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-1-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-1.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-1-hd.jpg",
       key: "d-1",
       status: 2,
     },
@@ -110,8 +110,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向东1格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-1.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-1-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-1.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-1-hd.jpg",
       key: "d-1",
       status: 1,
     },
@@ -120,8 +120,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向南3格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-2.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-2-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-2.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-2-hd.jpg",
       key: "d-2",
       status: 3,
     },
@@ -130,8 +130,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向南2格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-2.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-2-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-2.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-2-hd.jpg",
       key: "d-2",
       status: 2,
     },
@@ -140,8 +140,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向南1格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-2.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-2-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-2.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-2-hd.jpg",
       key: "d-2",
       status: 1,
     },
@@ -150,8 +150,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向西3格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-3.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-3-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-3.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-3-hd.jpg",
       key: "d-3",
       status: 3,
     },
@@ -160,8 +160,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向西2格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-3.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-3-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-3.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-3-hd.jpg",
       key: "d-3",
       status: 2,
     },
@@ -170,8 +170,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向西1格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-3.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-3-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-3.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-3-hd.jpg",
       key: "d-3",
       status: 1,
     },
@@ -180,8 +180,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向北3格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-4.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-4-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-4.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-4-hd.jpg",
       key: "d-4",
       status: 3,
     },
@@ -190,8 +190,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向北2格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-4.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-4-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-4.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-4-hd.jpg",
       key: "d-4",
       status: 2,
     },
@@ -200,8 +200,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "向北1格",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/d-4.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/d-4-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/d-4.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/d-4-hd.jpg",
       key: "d-4",
       status: 1,
     },
@@ -219,8 +219,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "国王",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/r-2.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/r-2-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/r-2.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/r-2-hd.jpg",
       key: "r-2",
       status: -1,
     },
@@ -229,8 +229,8 @@ export const propsConfig: PropConfig[] = [
   {
     data: {
       name: "王后",
-      imgUrl: "https://cdn.renwuming.cn/static/escape/r-3.jpg",
-      imgHdUrl: "https://cdn.renwuming.cn/static/escape/r-3-hd.jpg",
+      imgUrl: "https://cdn.renwuming.cn/static/escape/cards/r-3.png",
+      imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/r-3-hd.jpg",
       key: "r-3",
       status: -1,
     },
@@ -243,15 +243,15 @@ export const startLocations: number[] = [0, 7, 56, 63];
 export const selectShieldList: Prop[] = [
   {
     name: "刃",
-    imgUrl: "https://cdn.renwuming.cn/static/escape/e-2-0.jpg",
-    imgHdUrl: "https://cdn.renwuming.cn/static/escape/e-2-0-hd.jpg",
+    imgUrl: "https://cdn.renwuming.cn/static/escape/cards/e-2-0.png",
+    imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/e-2-0-hd.jpg",
     key: "e-2",
     status: 0,
   },
   {
     name: "盾",
-    imgUrl: "https://cdn.renwuming.cn/static/escape/e-2-1.jpg",
-    imgHdUrl: "https://cdn.renwuming.cn/static/escape/e-2-1-hd.jpg",
+    imgUrl: "https://cdn.renwuming.cn/static/escape/cards/e-2-1.png",
+    imgHdUrl: "https://cdn.renwuming.cn/static/escape/cards/e-2-1-hd.jpg",
     key: "e-2",
     status: 1,
   },
